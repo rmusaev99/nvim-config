c49bc4b3e612dd89b2768b9754581d1ef12be443
rmusaev <rmusaev1999@gmail.com>
2024-09-09 10:38:46 -0300

    init

---
+++ lua/plugins/indent-blackline.lua
@@ -0,0 +1,10 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  ---@module "ibl"
+  ---@type ibl.config
+  opts = {},
+  config = function(_, opts)
+    require("ibl").setup(opts)
+  end,
+}
---
+++ lua/plugins/null-ls.lua
@@ -0,0 +1,22 @@
+return {
+  "nvimtools/none-ls.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.completion.spell,
+        null_ls.builtins.formatting.dxfmt,
+      },
+    })
+
+    vim.keymap.set("n", "<C-l>", function() 
+      vim.lsp.buf.format({ async = true }) 
+    end, { noremap = true, silent = true })
+  end,
+}
+
+
